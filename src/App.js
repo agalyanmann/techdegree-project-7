d52d05b3a67fb4603221f7fbd4bf7c87e6b7f5ea
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2020-08-18 09:26:49 -0400

    add Photo component and link to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../src/treehouse.css';
 
 import Nav from './component/Nav';
+import Photo from './component/Photo';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Nav /> 
+      <Photo />
     </div>
   );
 }
