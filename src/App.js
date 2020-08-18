63e90ae2d299d2612803f7548c8b21773e0be9fc
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2020-08-18 14:13:33 -0400

    make App.js Stateful

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../src/treehouse.css';
 import apiKey from './config';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
 
 import Search from './component/Search';
 import Nav from './component/Nav';
 import Photo from './component/Photo';
 import NotFound from './component/NotFound';
 
-function App() {
-  return (
-    <div className="container">
-      <Search />
-      <Nav /> 
-      <Photo />
-      <NotFound />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Search />
+        <Nav />
+        <Photo />
+        <NotFound />
+      </div>
+    );
+  }
 }
 
 export default App;
