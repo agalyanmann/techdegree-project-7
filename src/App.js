b2d3d23a598aa0136e4930c6edaeed4b2c767186
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2020-08-18 13:17:36 -0400

    Revert "remove ul style from error page"
    This reverts commit bb987face337fe056d0a87fd6902e423fb20b721.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import '../src/treehouse.css';
 
-import Search from './component/Search';
 import Nav from './component/Nav';
 import Photo from './component/Photo';
-import NotFound from './component/NotFound';
+import Search from './component/Search';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Search />
       <Nav /> 
       <Photo />
-      <NotFound />
     </div>
   );
 }
